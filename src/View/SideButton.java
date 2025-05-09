5ce26741567f3f0e30c753c720df0b9862698914
mikalah17 <mikalahquisumbing@gmail.com>
2025-05-09 21:26:04 +0800

    Update SideButton.java
    login

--- src/View/SideButton.java
+++ src/View/SideButton.java
@@ -8,42 +8,50 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
 import javax.swing.JPanel;
+import javax.swing.JFrame;
 
 import model.Database;
 import model.User;
 
-import javax.swing.JLabel;
-
 @SuppressWarnings("serial")
 public class SideButton extends JPanel {
 
-	public SideButton(String text, String string, User user, Database database) {
+	public SideButton(String text, String action, User user, Database database) {
 		super(new FlowLayout(FlowLayout.LEFT, 10, 10));
 		setMaximumSize(new Dimension(182, 50));
 		setBackground(GUIConstants.white);
 		setCursor(new Cursor(Cursor.HAND_CURSOR));
-		
+
 		add(new View.JLabel(text, 17, GUIConstants.textAreaHint, Font.BOLD));
-		
+
 		addMouseListener(new MouseListener() {
-        	@Override
+			@Override
 			public void mouseReleased(MouseEvent e) {}
 			@Override
 			public void mousePressed(MouseEvent e) {}
 			@Override
 			public void mouseExited(MouseEvent e) {
 				setBackground(GUIConstants.white);
-				}
+			}
 			@Override
 			public void mouseEntered(MouseEvent e) {
 				setBackground(GUIConstants.hover);
-			
 			}
 			@Override
-			public void mouseClicked(MouseEvent e) 	{
-				new CustomView(text, user, database);
-	}
-		} );
+			public void mouseClicked(MouseEvent e) {
+				switch (action.toLowerCase()) {
+					case "logout":
+						JFrame currentFrame = (JFrame) getTopLevelAncestor();
+						if (currentFrame != null) {
+							currentFrame.dispose();
+						}
+						new Login(database);
+						break;
+					default:
+						System.out.println("Unhandled action: " + action);
+						break;
+				}
+			}
+		});
 	}
 }
-		
